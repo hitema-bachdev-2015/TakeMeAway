e3b47a6e41bcdc3d8a1a7e5960994f8c46148177
ValpeX <kevin.martin@quatdev.fr>
2015-01-21 01:00:49 +0100

    Update style

--- layout/accueil.php
+++ layout/accueil.php
@@ -10,7 +10,7 @@
       </div>
 
       <div class="row">
-        <div class="col-md-4" style="padding-top: 0;padding-bottom: 0;background-color: #FFF;border: 0;">
+        <div class="col-md-4" style="padding-top: 0;padding-bottom: 0;background-color: rgba(0, 0, 0, 0);border: 0;">
           <div class="row">
             <div class="col-md-12">ITINERAIRE
               <label><span>Départ :</span><input id="StartAddress" type="text" /></label>
@@ -21,8 +21,8 @@
               </ul>
               <label><span>Carburant :</span><select id="Carb"><option value="">Default</option><option value="">Essence</option><option value="">Diesel</option></select></label>
               <label>Transport :<select id="Trans"><option value="default">Default</option><option value="0">Voiture</option><option value="1">Break</option><option value="2">Camion</option></select></label>
-              <input id="btnGeocode" type="button" value="Lancer" />
-              <input id="btnAddToFavoris" type="button" value="Ajouter aux favoris" />
+              <input id="btnGeocode" type="button" value="Lancer" class="btn-style" />
+              <input id="btnAddToFavoris" type="button" value="Ajouter aux favoris" class="btn-style" />
           </div>
         </div>
         <div class="row">
@@ -46,7 +46,7 @@
           </div>
  </div>
        
-         <div class="col-md-1" style="padding-top: 15px;padding-bottom: 15px;background-color: #FFF;border: 0;">
+         <div class="col-md-1" style="padding-top: 15px;padding-bottom: 15px;background-color: rgba(0, 0, 0, 0);border: 0;">
         </div>
         <div class="col-md-7">
          </div>
