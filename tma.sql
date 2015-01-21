2738b8c02b9dab16feb26b5772cfe2d981aac543
ymjere <jeremyriverain@hotmail.fr>
2015-01-21 11:12:38 +0100

    Base de donnée mis a jour

--- tma.sql
+++ tma.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 19 Janvier 2015 à 14:08
+-- Généré le :  Mer 21 Janvier 2015 à 11:11
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -30,15 +30,14 @@ CREATE TABLE IF NOT EXISTS `historique` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_utilisateur` int(11) NOT NULL,
   `date` timestamp NOT NULL,
-  `rue_depart` varchar(255) NOT NULL,
-  `num_depart` int(11) NOT NULL,
-  `CP_depart` varchar(5) NOT NULL,
-  `rue_arrive` varchar(255) NOT NULL,
-  `num_arrive` int(11) NOT NULL,
-  `CP_arrive` varchar(5) NOT NULL,
+  `adresse_depart` varchar(255) NOT NULL,
+  `Latitude_depart` varchar(30) NOT NULL,
+  `longitude_depart` varchar(30) NOT NULL,
+  `adresse_arrive` varchar(255) NOT NULL,
+  `latitude_arrive` varchar(30) NOT NULL,
+  `longitude_arrive` varchar(30) NOT NULL,
   `id_vehicule` int(11) NOT NULL,
-  `duree` time NOT NULL,
-  `favori` int(11) NOT NULL DEFAULT '0',
+  `favori` smallint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
@@ -46,8 +45,8 @@ CREATE TABLE IF NOT EXISTS `historique` (
 -- Contenu de la table `historique`
 --
 
-INSERT INTO `historique` (`id`, `id_utilisateur`, `date`, `rue_depart`, `num_depart`, `CP_depart`, `rue_arrive`, `num_arrive`, `CP_arrive`, `id_vehicule`, `duree`, `favori`) VALUES
-(1, 0, '2015-01-19 13:04:21', 'rue de Saint Prix', 81, '95320', 'Rue Chateaudun', 27, '75009', 1, '00:14:56', 0);
+INSERT INTO `historique` (`id`, `id_utilisateur`, `date`, `adresse_depart`, `Latitude_depart`, `longitude_depart`, `adresse_arrive`, `latitude_arrive`, `longitude_arrive`, `id_vehicule`, `favori`) VALUES
+(1, 1, '2015-01-19 21:43:48', 'rue de Saint Prix', '81', '95320', 'Rue Chateaudun', '27', '75009', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -68,7 +67,27 @@ CREATE TABLE IF NOT EXISTS `tchat` (
 --
 
 INSERT INTO `tchat` (`id`, `id_utilisateur`, `date_envoi`, `msg`) VALUES
-(1, 0, '2015-01-19 13:05:24', 'Yo !\r\nCa va ?');
+(1, 1, '2015-01-19 21:43:32', 'Yo !\r\nCa va ?');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type_vehicule`
+--
+
+CREATE TABLE IF NOT EXISTS `type_vehicule` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `type_vehicule`
+--
+
+INSERT INTO `type_vehicule` (`id`, `libelle`) VALUES
+(1, 'voiture'),
+(2, 'camion');
 
 -- --------------------------------------------------------
 
@@ -81,10 +100,13 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `nom` varchar(255) NOT NULL,
   `prenom` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `MdP` varchar(255) NOT NULL,
+  `mdp` varchar(255) NOT NULL,
   `connecte` int(11) NOT NULL DEFAULT '0',
-  `token` varchar(255) NOT NULL,
   `date_subscrib` date NOT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `cp` varchar(5) NOT NULL,
+  `ville` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `token` (`token`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
@@ -93,8 +115,8 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
 -- Contenu de la table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `email`, `MdP`, `connecte`, `token`, `date_subscrib`) VALUES
-(1, 'Riverain', 'Jeremy', 'jeremyriverain@hotmail.fr', 'bretagne', 0, 'klsgqkjsdgfqsdkjgfqsdjkhfqkjshdgerghjklm', '2015-01-19');
+INSERT INTO `utilisateur` (`id`, `nom`, `prenom`, `email`, `mdp`, `connecte`, `date_subscrib`, `adresse`, `cp`, `ville`, `token`) VALUES
+(1, 'Riverain', 'Jeremy', 'jeremyriverain@hotmail.fr', 'bretagne', 0, '2015-01-19', '81 rue de Saint Prix', '95320', 'Saint Leu la Forêt', 'ksldhgqfqkshgdueygrfbxnkqbkbviouvb');
 
 -- --------------------------------------------------------
 
@@ -109,15 +131,17 @@ CREATE TABLE IF NOT EXISTS `vehicule` (
   `modele` varchar(255) NOT NULL,
   `consommation` float NOT NULL,
   `type_moteur` int(11) NOT NULL,
+  `type_vehicule` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
 --
 -- Contenu de la table `vehicule`
 --
 
-INSERT INTO `vehicule` (`id`, `id_utilisateur`, `marque`, `modele`, `consommation`, `type_moteur`) VALUES
-(1, 0, 'Audi', 'TT', 10, 0);
+INSERT INTO `vehicule` (`id`, `id_utilisateur`, `marque`, `modele`, `consommation`, `type_moteur`, `type_vehicule`) VALUES
+(1, 1, 'Audi', 'TT', 10, 1, 1),
+(2, 1, 'Peugeot', '106', 10, 1, 1);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
