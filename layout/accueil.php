181904f31161f6bc133c681c15d5149e6d570530
navec <navecbatchi@gmail.com>
2015-01-22 06:35:00 +0100

    css

--- layout/accueil.php
+++ layout/accueil.php
@@ -4,12 +4,11 @@
         <h1>TakeMeAway!</h1>
       </div>
 
-      
       <div class="row">
         <div class="col-md-12">
-        	<h4>Localiser une adresse</h4>
+        	<h4>LOCALISER UNE ADRESSE</h4>
           <div id="search">
-	            <input id="barreDeRecherche" type="text" />
+	            <input id="barreDeRecherche" type="text" placeholder='Rechercher...' />
 	            <ul id="autoCompleteBdr"></ul>
             </div>
         </div>
@@ -17,18 +16,41 @@
 
       <div class="row">
         <div class="col-md-4" style="padding-top: 0;padding-bottom: 0;background-color: rgba(0, 0, 0, 0);border: 0;">
+          <div class="row lienConnexion">
+            <div class="col-md-12"><h4><a href="connexion.php">Connexion</a>|<a href="inscription.php">Inscripsion</a></h4></div>
+          </div>
+
           <div class="row">
-            <div class="col-md-12"><h4><span>ITINERAIRE</span></h4>
-              <label for="StartAddress"><span>Départ :</span></label><input id="StartAddress" type="text" />
-              <ul id="autoComplete1">
-              </ul>
-              <label for="EnAddress"><span>Destination :</span></label><input id="EndAddress" type="text" />
-              <ul id="autoComplete2">
-              </ul>
-              <label for="Carb"><span>Carburant :</span></label><select id="Carb"><option value="">Default</option><option value="">Essence</option><option value="">Diesel</option></select>
-              <label for="Trans"><span>Transport :</span></label><select id="Trans"><option value="default">Default</option><option value="0">Voiture</option><option value="1">Break</option><option value="2">Camion</option></select>
-              <br><br>
-              <input id="btnGeocode" type="button" value="Lancer" class="btn-style" />
+            <div class="col-md-12">
+              <h4>ITINERAIRE</h4>
+              <label>
+                <span>Départ</span>:
+                <input id="StartAddress" type="text" />
+                <ul id="autoComplete1"></ul>
+              </label>
+                <label>
+                  <span>Destination</span>:
+                  <input id="EndAddress" type="text" />
+                </label>
+              <ul id="autoComplete2"></ul>
+              <label>
+                <span>Carburant </span>:
+                <select id="Carb">
+                  <option value="">Default</option>
+                  <option value="">Essence</option>
+                  <option value="">Diesel</option>
+                </select>
+              </label>
+              <label>
+                <span>Transport </span>:
+                <select id="Trans">
+                  <option value="default">Default</option>
+                  <option value="0">Voiture</option>
+                  <option value="1">Break</option>
+                  <option value="2">Camion</option>
+                </select>
+              </label>
+              <input id="btnGeocode" type="button" value="Lancer" class="btn-style bouton" />
               </div>
         </div>
         <div class="row">
@@ -56,7 +78,7 @@
         </div>
         <div class="col-md-7">
          </div>
-           <div class="col-md-1" style="padding-top: 15px;padding-bottom: 15px;background-color: #FFF;border: 0;">
+           <div class="col-md-1" style="padding-top: 15px;padding-bottom: 15px;background-color: #f9f9f9;border: 0;">
         </div>
           
        
