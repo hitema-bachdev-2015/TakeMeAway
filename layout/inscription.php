f675c418228b7f1af546c1693fe11ca43bec3708
Yannick <yannick.elusue@hotmail.fr>
2015-01-23 15:57:31 +0100

    Inscription champs obligatoires + favicon

--- layout/inscription.php
+++ layout/inscription.php
@@ -9,34 +9,35 @@ if(isset($error_mail) && $error_mail == "error"){ ?>
 
 	<form action="#" method="POST" id="formInscription">
 		<label>
-			<span>Nom</span>:
+			<span>* Nom</span>:
 			<input type="text" name="nom" value="<?php echo isset($_POST['nom']) ? $_POST['nom'] : ''; ?>" required/>
 		</label>
 		<label>
-			<span>Prenom</span>:
+			<span>* Prenom</span>:
 			<input type="text" name="prenom"  value="<?php echo isset($_POST['prenom']) ? $_POST['prenom'] : ''; ?>" required/>
 		</label>
 		<label>
-			<span>E-mail</span>:
+			<span>* E-mail</span>:
 			<input type="text" name="mail"  value="<?php echo isset($_POST['mail']) ? $_POST['mail'] : ''; ?>" required/>
 			<?php echo isset($error_mail) && $error_mail == "error" ? "<img src='img/icones/error.png' width='25px' />" : "" ?>
 		</label>
 		<label>
-			<span>Mot de passe</span>:
+			<span>* Mot de passe</span>:
 			<input type="password" name="mdp"  value="<?php echo isset($_POST['mdp']) ? $_POST['mdp'] : ''; ?>" required/>
 		</label>
 		<label>
-			<span>Adresse</span>:
+			<span>* Adresse</span>:
 			<input type="text" name="adresse"  value="<?php echo isset($_POST['adresse']) ? $_POST['adresse'] : ''; ?>" required/>
 		</label>
 		<label>
-			<span>Code Postal</span>:
+			<span>* Code Postal</span>:
 			<input type="text" name="cp"  value="<?php echo isset($_POST['cp']) ? $_POST['cp'] : ''; ?>" required/>
 		</label>
 		<label>
-			<span>Ville</span>:
+			<span>* Ville</span>:
 			<input type="text" name="ville"  value="<?php echo isset($_POST['ville']) ? $_POST['ville'] : ''; ?>" required/>
 		</label>
+		<p>* : Champ obligatoire</p>
 		<div class="g-recaptcha" data-sitekey="6Ld0uwATAAAAAHZJhd-kkJ-lw2HgJGSZWKCyRhFe"></div>
 		<?php echo isset($error_captcha) && $error_captcha == "error" ? "<img src='img/icones/error.png'  class='errorCaptcha' width='25px' />" : "" ?>
 		<p>
