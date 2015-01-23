0a4f903420b81cf76fc79dffd4f7ec8d74782f25
amadd <dabitaoamadou@gmail.com>
2015-01-23 16:10:16 +0100

    Mentions Légales
    Mentions Légales
    V.1

---
+++ mentionslegales.php
@@ -0,0 +1,175 @@
+<HTMl>
+
+  <HEAD>
+      <TITLE>Mentions Légales</TITLE>
+      <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+
+
+  <!-- insertion de Query -->
+  <script type="text/javascript" src="js/jquery-2.1.1.min.js"></script>
+  <script type="text/javascript" src="js/jquery-ui.min.js"></script>
+
+
+    <!-- insertion de Google JSapi -->
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+  
+  <!-- insertion de mes scripts JS -->
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+  <!-- insertion des liens css -->
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+  <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/grid.css" rel="stylesheet">
+
+    <style type="text/css">
+    #ml{
+      text
+      background-color: white;
+      width: 100%;
+      height: 200px;
+      padding-top: 50px;
+
+    }
+
+    </style>
+  </HEAD>
+
+
+
+  <BODY>
+  <H1> Mentions Légales</H1>
+    <div class="row">
+        <div class="col-md-1" style="border:0px;"></div>
+        <div class="col-md-10">
+        <pre>
+        Propriétaire 
+TakeMeAway! (Établissement public à caractère industriel et commercial)
+RCS de Paris n° 775 663 438
+54, quai de la Rapée
+75599 PARIS CEDEX 12
+Tél. : +33 1 58 78 20 20
+
+Données à caractère personnel
+Les données recueillies ou traitées à l’occasion de votre navigation sur le présent site (ci-après, le « Site ») sont destinées à la TakeMeAway!, ses sous-traitants et partenaires, afin d’améliorer les services offerts sur le Site, de faciliter votre navigation sur le Site et de traiter vos requêtes, de procéder à la création de votre compte, le cas échéant, et de vous informer de l’actualité de la TakeMeAway! en matière d’open data, sauf opposition de votre compte.
+
+Cookies
+Lors de l'utilisation du Site, des informations relatives à la navigation de votre terminal (ordinateur, tablette, smartphones, etc.) (ci-après, le « Terminal ») sont susceptibles d’être enregistrées ou accédées dans des fichiers « cookies » installés sur votre Terminal, sous réserve des choix que vous aurez exprimés concernant les cookies et que vous pourrez modifier à tout moment (Voir la rubrique « Vos choix concernant les cookies » ci-dessous). 
+
+Qu’est-ce qu’un cookie ?
+Un cookie est un fichier que nous pouvons être amenés à enregistrer sur le disque dur de votre Terminal par le biais de votre navigateur Internet lorsque vous utilisez le Site, sous réserve de votre accord ou sauf opposition de votre part.
+
+A quoi servent les cookies émis sur le Site ? 
+Seul l’émetteur d’un cookie est susceptible de lire ou de modifier des informations qui y sont contenues. 
+Lorsque vous vous connectez et utilisez le Site, nous, nos sous-traitants et des tiers annonceurs, pouvons être amenés, sous réserve de vos choix, à installer divers cookies dans votre Terminal nous permettant de reconnaître le navigateur de votre Terminal pendant la durée de validité du cookie concerné. Les cookies sont utilisés aux fins décrites ci-dessous, sous réserve de vos choix que vous pouvez exprimer et modifier à tout moment via les paramètres du logiciel de navigation utilisé lors de votre navigation sur le Site.
+Les cookies émis sur le Site permettent :
+- d’adapter la présentation du Site aux préférences d’affichage de votre Terminal (langue utilisée, résolution d’affichage, système d’exploitation utilisée, etc.) lors de votre utilisation du Site, selon les matériels et logiciels de visualisation ou de lecture que votre Terminal comporte ;
+- d’établir des statistiques et mesures de fréquentation et d’utilisation des divers éléments composant le Site (rubriques et contenus visités, parcours, etc.), nous permettant d’améliorer l’intérêt et l’ergonomie du Site ;
+- de mémoriser des informations relatives à un formulaire que vous avez rempli lors de l'utilisation du Site (inscription ou accès à votre compte) ; et
+- d’adapter les contenus informationnels ou promotionnels du Site en fonction de vos intérêts et préférences supposés, déclarés, ou résultant de votre navigation sur le Site, et en fonction de votre lieu de connexion au Site (pays, région ou ville).
+La régie publicitaire Addict Media (ci-après, la « Régie ») émet des cookies sur le Site aux fins décrites ci-dessus, ainsi qu’à à des fins de comptage et de mesure de réactivité aux insertions publicitaires sur le Site. 
+La Régie peut elle-même avoir recours à des prestataires et partenaires annonceurs pour animer les espaces promotionnels ou publicitaires du Site. Toutefois, la TakeMeAway! ne dispose d’aucun lien contractuel avec lesdits partenaires ou plateformes d’intermédiation publicitaire et ne dispose d’aucun contrôle sur leurs activités. En conséquence, il vous appartient de paramétrer votre navigateur afin de gérer l’émission de ces cookies (voir la rubrique « Vos choix concernant les cookies » ci-dessous).
+Les cookies émis sur le Site sont des cookies de session (dont la durée est limitée au temps d’une connexion au Site) et des cookies persistants (dont la durée, toutefois limitée, est supérieure à la durée d’une connexion). 
+Les cookies de session ne sont actifs que le temps de votre visite et sont supprimés lorsque vous fermez votre navigateur. Les cookies persistants restent enregistrés sur le disque dur de votre Terminal une fois votre navigateur fermé jusqu’à ce que vous-mêmes ou votre logiciel de navigation les purge après un certain délai ou à tout moment.
+
+Vos choix concernant les cookies : 
+Plusieurs possibilités vous sont offertes pour gérer les cookies. Tout paramétrage que vous pouvez entreprendre sera susceptible de modifier votre navigation sur Internet et vos conditions d’accès à certains services nécessitant l’utilisation de cookies.
+Vous pouvez configurer votre logiciel de navigation de manière à ce que les cookies soient enregistrés dans votre Terminal ou, au contraire, rejetés, soit systématiquement, soit selon leur émetteur. Vous pouvez également configurer votre logiciel de navigation de manière à ce que l’acceptation ou le refus des cookies vous soient proposés ponctuellement, avant qu’un cookie soit susceptible d’être enregistré sur votre Terminal. 
+Le menu d'aide de votre navigateur vous permettra de savoir de quelle manière exprimer ou modifier vos préférences en matière de cookies :
+
+Pour Internet Explorer™ : windows.microsoft.com/fr-FR/windows-vista/Block-or-allow-cookies,
+
+Pour Safari™ : support.apple.com/kb/ht1677,
+
+Pour Chrome™ : support.google.com/chrome/bin/answer.py,
+
+Pour Firefox™ : support.mozilla.org/fr/kb/Activer%20et%20d%C3%A9sactiver%20les%20cookies,
+
+Pour Opera™ : help.opera.com/Windows/10.20/fr/cookies.html.
+
+Nous utilisons notamment des cookies "techniques" nécessaires au bon fonctionnement du Site. Si vous refusez l’enregistrement de cookies dans votre Terminal, ou si vous supprimez ceux qui y sont enregistrés, vous ne pourrez plus bénéficier d’un certain nombre de fonctionnalités qui sont néanmoins nécessaires pour naviguer dans certains espaces du Site. 
+Le cas échéant, nous déclinons toute responsabilité pour les conséquences liées au fonctionnement dégradé du Site résultant de l’impossibilité pour nous d’enregistrer ou de consulter les cookies nécessaires à leur fonctionnement et que vous auriez refusés ou supprimés.
+
+Correspondant à la protection des données 
+Conformément à la loi n° 78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés, la TakeMeAway! a désigné un correspondant à la protection des données à caractère personnel. Sa désignation est effective depuis le 10 avril 2008. 
+En application de l'alinéa IV de l'article 22 de la loi n° 78-17, la TakeMeAway! communique à toute personne qui en fait la demande les informations relatives aux traitements de données à caractère personnel qu'elle met en œuvre.
+
+Droit d'accès et de rectification
+Conformément à la loi n° 78-17 du 6 janvier 1978, vous bénéficiez d'un droit d'accès à ces informations et, le cas échéant, du droit de rectification, ainsi que d'un droit d'opposition à leur conservation ou à leur utilisation à des fins promotionnelles. 
+
+Si vous souhaitez exercer ce droit, il vous suffit de nous contacter en joignant à votre demande la copie d'une pièce d'identité par courrier postal : 
+
+Correspondant Informatique et Libertés de la TakeMeAway! 
+13, rue Jules Vallès 
+LAC JV27 
+75547 PARIS CEDEX 11
+
+
+Propriété intellectuelle
+A l'exception des données téléchargeables sous licence spécifique, l'intégralité de data.TakeMeAway!.fr est protégée par les législations françaises et internationales relatives à la propriété intellectuelle. Tous les droits de reproduction sont réservés, y compris pour les documents téléchargeables. L'ensemble des textes, graphismes, icônes, photographies, plans, logos, vidéos, sons, marques (.) et plus généralement l'ensemble des éléments composant le site de la TakeMeAway! et data.TakeMeAway!.fr peuvent, conformément à l'article L122-4 du code de la propriété intellectuelle, faire l'objet d'une quelconque représentation ou reproduction, intégrale ou partielle, sur quelque support que ce soit, sans l'autorisation expresse et préalable de la TakeMeAway!.
+
+Le non-respect de cette interdiction constitue un acte de contrefaçon pouvant engager la responsabilité civile et/ou pénale de son auteur. La TakeMeAway! se réserve le droit d'engager des poursuites judiciaires à l'encontre de toute personne qui n'aurait pas respecté cette interdiction.
+
+De même, il est strictement interdit d'utiliser ou de reproduire le nom "TakeMeAway!" et/ou son logo, seuls ou associés, à quelque titre que ce soit et sur quelque support que ce soit sans l'accord préalable et écrit de la TakeMeAway!. 
+
+
+Loi applicable
+Les présentes Mentions Légales, le Site et toutes conséquences de son utilisation ou de sa disponibilité ou de son indisponibilité sont régis par la loi française. 
+Responsabilité de la TakeMeAway! 
+Les informations communiquées sur le site data.TakeMeAway!.fr sont fournies à titre indicatif, elles sont non contractuelles et ne sauraient engager la responsabilité de la TakeMeAway!. Elles peuvent être modifiées ou mises à jour sans préavis. La TakeMeAway! se réserve également le droit, à tout moment et sans préavis, d'apporter des améliorations et/ou modifications au site data.TakeMeAway!.fr.
+
+La responsabilité de la TakeMeAway! ne saurait être engagée pour :
+
+- les dommages de toute nature, directs ou indirects, résultant de l'utilisation du site data.TakeMeAway!.fr et notamment toute perte d'exploitation, perte financière ou commerciale, perte de programmes et/ou de données en particulier dans le système d'information de l'utilisateur du site data.TakeMeAway!.fr,
+- les dommages de toute nature, directs ou indirects, résultant du contenu et/ou de l'utilisation des sites Internet liés au site data.TakeMeAway!.fr ou auxquels les utilisateurs pourraient avoir accès via le site data.TakeMeAway!.fr,
+- l'impossibilité d'accéder au site data.TakeMeAway!.fr et/ou à l'un des sites Internet lui étant lié,
+- les omissions et/ou erreurs que pourrait contenir le site data.TakeMeAway!.fr.
+
+
+Reproduction de contenus
+Sous réserve des règles particulières applicables aux contenus téléchargeables sur le Site , les utilisateurs du Site ne peuvent reproduire de contenus (textes, photos, vidéos, sons, animations, etc.), données ou extraits de bases de données ou tous éléments logiciels qui le composent ou y sont accessibles, sans l'autorisation expresse et préalable de la TakeMeAway!. 
+Aussi, pour toute demande de reproduction d'éléments contenus dans le Site, merci de nous adresser un message.
+Utilisateurs
+
+L'utilisateur du présent site est responsable des dommages de toute nature, matériels ou immatériels, directs ou indirects, causés à tout tiers, y compris à la TakeMeAway!, du fait de l'utilisation ou de l'exploitation illicite du site data.TakeMeAway!.fr lui-même et/ou de l'un de ses éléments, quels que soient la cause et le lieu de survenance de ces dommages, et garantit la TakeMeAway! des conséquences des réclamations ou actions dont elle pourrait, de ce fait, faire l'objet. 
+L'utilisateur du site de la TakeMeAway! renonce à exercer tout recours contre la TakeMeAway! dans le cas de poursuites diligentées par un tiers à son encontre du fait de l'utilisation et/ou de l'exploitation illicite du site data.TakeMeAway!.fr. 
+Gestionnaire de contenu: 
+Le site data.TakeMeAway!.fr est développé avec le gestionnaire de contenu TYPO3. 
+Le code source de la plateforme opendata est publié sous licence GNU GPL v3.
+
+
+Animation éditoriale du site:
+TakeMeAway! - Département commercial
+54, quai de la Rapée
+75599 PARIS CEDEX 12
+(Aurélien Serrault)
+
+Réalisation du site: 
+Plan.Net
+RCS Nanterre 432 824 027
+Code APE : 7311Z
+Siège social : 114 Rue Chaptal, 92300 Levallois Perret 
+
+Site hébergé par :
+Ikoula
+175-177 rue d'Aguesseau 
+92100 Boulogne Billancourt
+        </pre>
+        </div>
+        <div class="col-md-1" style="border:0px;"></div>
+      
+          
+       
+  </div>
+   
+
+    
+  </BODY>
+</HTML>
